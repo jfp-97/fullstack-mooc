cd1a63dd2e2c8afcbe5be9f5b4f5f3c3b04f6330
Juan Francisco Perez <juanf.perez.97@gmail.com>
2021-05-04 14:29:29 -0300

    e2.5

---
+++ part2/course-information/src/components/Course.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part =>
+        <Part part={part} key={part.name} />
+      )}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Total = ({ course }) => {
+  return (
+    <b>
+      total of {course.parts.reduce((a, b) => a + b.exercises, 0)} exercises
+    </b>
+  ) 
+}
+
+export default Course
